592b15e027122b63f4bd3c4fbed45d4adb1b6e58
h4x0rlol <mazafaker.com@gmail.com>
2021-07-22 00:06:49 +0600

    template setup and header

---
+++ client/pages/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import styles from "../styles/Index.module.scss";
+
+const Index = () => {
+  return (
+    <div className={styles.container}>
+      <Header />
+    </div>
+  );
+};
+
+export default Index;
