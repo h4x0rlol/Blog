7f945fc66c01963104cd910b15bc87efc47c2ccb
h4x0rlol <mazafaker.com@gmail.com>
2021-07-25 23:13:59 +0600

    blog page

--- client/pages/blog.jsx
+++ client/pages/blog.jsx
@@ -1,5 +1,38 @@
+import styles from "../styles/Blog.module.scss";
+
 const Blog = () => {
-  return <div>Blog page</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <article className={styles.article}>
+          <div className={styles.article_inner}>
+            <div className={styles.wrapper}>
+              <div>
+                <header className={styles.article_header}>
+                  <h1 className={styles.name}>
+                    <a className={styles.article_title}>
+                      There is no posts yet
+                    </a>
+                  </h1>
+                </header>
+              </div>
+              <div>
+                <div className={styles.article_date}>
+                  <time className={styles.time}>July 25, 2021</time>
+                </div>
+              </div>
+            </div>
+            <div className={styles.article_footer}>
+              <ul className={styles.article_tags}>
+                <li className={styles.tags_first}>Tags:</li>
+                <li className={styles.tags_item}>None</li>
+              </ul>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
