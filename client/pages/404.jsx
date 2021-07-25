aeff2766fed3c9b544e2d067acdb3444232149d0
h4x0rlol <mazafaker.com@gmail.com>
2021-07-25 22:36:53 +0600

    404 page

--- client/pages/404.jsx
+++ client/pages/404.jsx
@@ -1,5 +1,14 @@
+import styles from "../styles/404.module.scss";
+
 const Custom404 = () => {
-  return <div>not found</div>;
+  return (
+    <div className={styles.error}>
+      <h1 className={styles.h1}>404</h1>
+      <div className={styles.text}>
+        <h2 className={styles.h2}>This page could not be found.</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Custom404;
