b6e2db53b539a4f8a92a1fdc7f38d5b89cea5663
h4x0rlol <mazafaker.com@gmail.com>
2021-07-27 21:26:08 +0600

    fix page flashing

---
+++ client/pages/_document.jsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const setInitialTheme = `
+    document.body.dataset.theme = 'dark';
+  `;
+    return (
+      <Html>
+        <Head />
+        <body>
+          <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
