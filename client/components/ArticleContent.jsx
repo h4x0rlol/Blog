a123312958a758160f9f0d6a8d181c0547cac1ab
h4x0rlol <mazafaker.com@gmail.com>
2022-09-01 22:01:24 +0600

    feat: add scroll progress

--- client/components/ArticleContent.jsx
+++ client/components/ArticleContent.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Article.module.scss";
+import ReadProgress from "./ReadProgress";
 
 const ArticleContent = ({ article }) => {
+  const [scrollPercent, setScrollPercent] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const handleScroll = () => {
+        const percent =
+          (window.scrollY / (ref.current.clientHeight - window.innerHeight)) *
+          100;
+        setScrollPercent(percent);
+      };
+
+      window.addEventListener("scroll", handleScroll, { passive: true });
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(scrollPercent);
+  }, [scrollPercent]);
+
   return (
-    <article
-      className={styles.container}
-      dangerouslySetInnerHTML={{ __html: article.content }}
-    ></article>
+    <>
+      <ReadProgress width={scrollPercent} />
+      <article
+        ref={ref}
+        className={styles.container}
+        dangerouslySetInnerHTML={{ __html: article.content }}
+      />
+    </>
   );
 };
 
