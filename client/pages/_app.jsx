87dc2c7e8acc755d15fa233929a5d2e45727350d
h4x0rlol <mazafaker.com@gmail.com>
2022-10-06 22:44:17 +0600

    feat: change favicons

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,12 +1,18 @@
+import Head from "next/head";
 import Layout from "../components/Layout";
 import "../styles/global.scss";
 import "../styles/Markdown.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
