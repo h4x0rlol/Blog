206fa6591ebe11f9eab5f32fb01e0c493755a37b
h4x0rlol <mazafaker.com@gmail.com>
2022-11-28 17:50:52 +0600

    feat: move to next/image

---
+++ client/components/CustomImage.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const CustomImage = ({ src, alt, width, height, alt }) => {
+  return <Image src={src} alt={alt} width={width} height={height} />;
+};
+
+CustomImage.defaultProps = {
+  alt: "",
+  width: 610,
+  height: 270,
+};
+
+export default CustomImage;
