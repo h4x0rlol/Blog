07f1021e21d0a2ac9982425cfda82e322d09b29d
h4x0rlol <mazafaker.com@gmail.com>
2022-12-06 19:48:31 +0600

    feat: add author meta

--- client/articles/Articles.js
+++ client/articles/Articles.js
@@ -2,6 +2,7 @@ export const Articles = [
   {
     id: 1,
     title: "Watch some metrics using Raspberry and React",
+    author: "Dmitriy Khailov",
     link: "react-raspberry-dashboard",
     tags: [
       "Raspberry Pi",
@@ -16,6 +17,7 @@ export const Articles = [
   {
     id: 2,
     title: "Setting up Ryzen CPUs on Arch Linux (and others)",
+    author: "Dmitriy Khailov",
     link: "archlinux-ryzen-setup",
     tags: ["Linux", "Arch", "Ryzen", "Voltage", "Noise", "Sensors"],
     date: "August 22, 2021",
