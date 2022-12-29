164c30ca6f99eb81667a81a9d2253e632e6cf334
h4x0rlol <mazafaker.com@gmail.com>
2022-12-30 00:16:07 +0600

    chore: code refactoring

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import { faArrowRight, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import styles from "/styles/Navbar.module.scss";
 
-export const Navbar = ({ children }) => {
+export const Navbar = () => {
   const router = useRouter();
   const [activeTheme, setActiveTheme] = useState(document.body.dataset.theme);
   const [isChecked, setIsChecked] = useState(
@@ -18,79 +18,75 @@ export const Navbar = ({ children }) => {
     setActiveTheme(inactiveTheme);
   };
 
-  useEffect(() => {
+  const onKeyDown = (e) => {
+    e.preventDefault();
+    if (e.key === " " || e.key === "Spacebar" || e.key === "Enter") {
+      handleThemeChange();
+    }
+  };
+
+  useLayoutEffect(() => {
     document.body.dataset.theme = activeTheme;
     window.localStorage.setItem("theme", activeTheme);
   }, [activeTheme]);
 
   return (
-    <>
-      <>
-        <header className={styles.navbar}>
-          <div className={styles.navbar_container}>
-            <h1 className={styles.navbar_name}>h4x0rlol</h1>
-            <nav className={styles.navbar_buttons}>
-              <Link
-                className={
-                  router.pathname == "/blog"
-                    ? styles.active_link
-                    : styles.inactive_link
-                }
-                href="/blog"
-              >
-                Blog
-              </Link>
-              <Link
-                className={
-                  router.pathname == "/"
-                    ? styles.active_link
-                    : styles.inactive_link
-                }
-                href="/"
-              >
-                About
-              </Link>
-            </nav>
-            <div className={styles.navbar_buttons}>
-              <div
-                className={styles.navbar_theme}
-                aria-label={`Change to ${inactiveTheme} mode`}
-                title={`Change to ${inactiveTheme} mode`}
-                type="button"
-                tabIndex="0"
-                onKeyPress={(e) => {
-                  e.preventDefault();
-                  if (e.key === " " || e.key === "Spacebar") {
-                    handleThemeChange();
-                  }
-                }}
-              >
-                <label className={styles.navbar_theme_switcher}>
-                  <input
-                    type="checkbox"
-                    checked={isChecked}
-                    onChange={handleThemeChange}
+    <header className={styles.navbar}>
+      <div className={styles.navbar_container}>
+        <h1 className={styles.navbar_name}>h4x0rlol</h1>
+        <nav className={styles.navbar_buttons}>
+          <Link
+            className={
+              router.pathname == "/blog"
+                ? styles.active_link
+                : styles.inactive_link
+            }
+            href="/blog"
+          >
+            Blog
+          </Link>
+          <Link
+            className={
+              router.pathname == "/" ? styles.active_link : styles.inactive_link
+            }
+            href="/"
+          >
+            About
+          </Link>
+        </nav>
+        <div className={styles.navbar_buttons}>
+          <div
+            className={styles.navbar_theme}
+            aria-label={`Change to ${inactiveTheme} mode`}
+            title={`Change to ${inactiveTheme} mode`}
+            type="button"
+            tabIndex="0"
+            onKeyDown={onKeyDown}
+          >
+            <label className={styles.navbar_theme_switcher}>
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={handleThemeChange}
+              />
+              <div>
+                <i className={styles.switcher_sun}>
+                  <FontAwesomeIcon icon={faSun} />
+                </i>
+                <i>
+                  <FontAwesomeIcon
+                    icon={faArrowRight}
+                    className={styles.switcher_arrow}
                   />
-                  <div>
-                    <i className={styles.switcher_sun}>
-                      <FontAwesomeIcon icon={faSun} />
-                    </i>
-                    <i>
-                      <FontAwesomeIcon
-                        icon={faArrowRight}
-                        className={styles.switcher_arrow}
-                      />
-                    </i>
-                    <i className={styles.switcher_moon}>
-                      <FontAwesomeIcon icon={faMoon} />
-                    </i>
-                  </div>
-                </label>
+                </i>
+                <i className={styles.switcher_moon}>
+                  <FontAwesomeIcon icon={faMoon} />
+                </i>
               </div>
-            </div>
+            </label>
           </div>
-        </header>
-      </>
-    </>
+        </div>
+      </div>
+    </header>
   );
 };
